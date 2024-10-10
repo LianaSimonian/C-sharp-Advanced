f11015d0c1b290009f55027e635de61d6e00c95f
Liana Simonian <liana.simoniian@gmail.com>
2024-10-10 17:06:34 +0300

    generics

---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book { Isbn = "1111", Title = "C# advanced" };
+
+            // var numbers = new List();
+            // numbers.Add(1);
+
+            // var books = new BookList();
+            // books.Add(book);
+
+            var numbers = new GenericList<int>();
+            numbers.Add(10);
+
+            var books = new GenericList<Book>();
+            books.Add(book);
+
+            var dictionary = new GenericDictionary<string, Book>();
+            dictionary.Add("1234",book);
+
+            var number = new Nullable<int>();
+            Console.WriteLine("Has Value ? " + number.HasValue);
+            Console.WriteLine("Value " +number.GetValueOrDefault());
+
+        }
+    }
+}
