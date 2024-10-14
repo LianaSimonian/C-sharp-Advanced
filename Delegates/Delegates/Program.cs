b1d4a1579913f9f116d30c6d2ebcbe1e4c297c91
Liana Simonian <liana.simoniian@gmail.com>
2024-10-14 18:22:59 +0300

    custom delegates

---
+++ Delegates/Delegates/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Delegates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            var processor = new PhotoProcessor();
+            var filters = new PhotoFilters();
+            PhotoProcessor.PhotoFilterHandler filterHandler = filters.ApplyBrightness;
+            filterHandler += filters.ApplyContrast;
+            filterHandler += RemoveRedEyeFilter;
+
+            processor.Process("image.jpg",filterHandler);
+        }
+
+        static void RemoveRedEyeFilter(Photo photo)
+        {
+            Console.WriteLine("Apply removeRedEye");
+        }
+    }
+}
