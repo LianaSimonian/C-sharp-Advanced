509f12228ff8bb9ff987a2d87f3c265b472abca4
Liana Simonian <liana.simoniian@gmail.com>
2024-10-15 20:30:17 +0300

    lamda expressions

---
+++ LambdaExpressions/LambdaExpressions/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LambdaExpressions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int> square = number => number * number;
+
+            Console.WriteLine(square(5));
+
+            const int factor = 5;
+            Func<int, int> multiplier = n => n * factor;
+            var result = multiplier(10);
+            Console.WriteLine(result);
+
+            var books = new BookRepository().GetBooks();
+            var cheapBooks = books.FindAll(b => b.Price<10);
+        }
+    }
+}
