029c9948ae810cb16198f5e865570964c7bfd719
Liana Simonian <liana.simoniian@gmail.com>
2024-10-22 12:58:02 +0300

    dynamic keyword

---
+++ DynamicBinding/DynamicBinding/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DynamicBinding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //static binding
+            object obj = new Book();
+            obj.GetHashCode();
+
+            //reflection
+            var methodInfo = obj.GetType().GetMethod("GetHashCode");
+            var result = methodInfo.Invoke(obj,null);
+            Console.WriteLine(result);
+
+            //dynamic binding
+            dynamic excelObject = "Mosh";
+            //excelObject.Optimize();
+
+            dynamic name = "Mosh";
+            name = 1;
+
+            int i = 1;
+            dynamic d = i;
+            long l = d;
+        }
+    }
+}
