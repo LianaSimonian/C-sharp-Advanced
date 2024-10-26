480f78c80ba92e39034d53ee19227621eb69a484
Liana Simonian <liana.simoniian@gmail.com>
2024-10-26 13:03:23 +0300

    Exception handling

---
+++ ExceptionHandling/ExceptionHandling/Calculator.cs
@@ -0,0 +1,11 @@
+﻿
+namespace ExceptionHandling
+{
+    internal class Calculator
+    {
+        public int Divide(int numerator, int denominator)
+        {
+            return numerator/denominator;
+        }
+    }
+}
